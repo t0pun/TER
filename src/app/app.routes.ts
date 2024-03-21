b1cc3c2c5925b8186baafd8ebeea61afe5ff5df5
thomas <thomas.laguerre@etu.umontpellier.fr>
2024-03-21 17:45:44 +0100

    angular creation page acceuil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { PagePrincipaleComponent } from './page-principale/page-principale.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+const routeConfig: Routes = [
+    {
+      path: '',
+      component: PagePrincipaleComponent,
+      title: 'Home page'
+    },
+    {
+      path: 'apropos',
+      component: AboutComponent,
+      title: 'A propos'
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        title: 'Dashboard'
+    },
+    {
+        path: 'search',
+        component: SearchComponent,
+        title: 'Search'
+      }
+
+  ];
+  
+  export default routeConfig;
