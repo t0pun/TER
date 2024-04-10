4d8646a47caae2bdb75c77ab7e449e473564791b
SateaMall <sate3.mallouh@gmail.com>
2024-04-10 17:45:26 +0200

    créer component resume et une service sans l'implémenter ou la remplir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  title="ClaimsKG statistical"
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,14 @@ import { Graph1Component } from './graph1/graph1.component';
 import { Graph2Component } from './graph2/graph2.component';
 import { Graph3Component } from './graph3/graph3.component';
 import { Graph4Component } from './graph4/graph4.component';
+import { ResumeComponent } from "../resume/resume.component";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [Graph1Component,Graph2Component,Graph3Component,Graph4Component],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.css',
+    imports: [Graph1Component, Graph2Component, Graph3Component, Graph4Component, ResumeComponent]
 })
 export class DashboardComponent {
 
