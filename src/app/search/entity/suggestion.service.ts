6054a2469406cf4f36b693a692c6b81bfafc1a24
SateaMall <sate3.mallouh@gmail.com>
2024-04-30 12:56:26 +0200

    finally working

--- src/app/search/entity/suggestion.service.ts
+++ src/app/search/entity/suggestion.service.ts
@@ -6,14 +6,12 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SuggestionService {
-  getFilteredEntities(term: string) {
-    throw new Error('Method not implemented.');
-  }
 
   constructor(private http: HttpClient) { }
 
   getSuggestions(query: string): Observable<string[]> {
-    console.log(this.http.get<string[]>('http://localhost:5000/suggestions', { params: { query } }));
-    return this.http.get<string[]>('http://localhost:5000/suggestions', { params: { query } });
+    const result= this.http.get<string[]>('http://localhost:5000/suggestions', { params: { query } });
+    console.log(result);
+    return result;
   }
 }
