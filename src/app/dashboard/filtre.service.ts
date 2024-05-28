31ce917d77f15fe08701d7ebef0a62e65f875fee
acslwt <ayoub.chenini.info@gmail.com>
2024-05-28 22:19:53 +0200

    update filter + entity filter

--- src/app/dashboard/filtre.service.ts
+++ src/app/dashboard/filtre.service.ts
@@ -40,19 +40,37 @@ export class FiltreService {
     this.submitTriggeredSource.next();
   }
 
-  onSubmit(granularite: string,debut: string, fin: string){
-    if(debut.length>0 && fin.length>0){
-      this.apiUrl_first_graph = "http://127.0.0.1:5000/json_per_date1_label/"+debut+"/"+fin+granularite
-    }else{
-      this.apiUrl_first_graph = "http://127.0.0.1:5000/json_per_date1_label/1996/2024/"+granularite
+  onSubmit(selectedOption: string[],granularite: string,debut: string, fin: string){
+    if(selectedOption.length==0){
+      if(debut.length>0 && fin.length>0){
+        this.apiUrl_first_graph = "http://127.0.0.1:5000/json_per_date1_label?date1="+debut+"&date2="+fin+"&granularite="+granularite
+      }else{
+        this.apiUrl_first_graph = "http://127.0.0.1:5000/json_per_date1_label?date1=1996&date2=2024&granularite="+granularite
+      }
+  
+      this.apiUrl_top_entity_graph = "http://127.0.0.1:5000/json_per_entity_date1_date2/"+debut+"/"+fin
+      this.apiUrl_per_source_label = "http://127.0.0.1:5000/json_born_per_source_label/"+debut+"/"+fin
+      this.apiUrl_per_langue = "http://127.0.0.1:5000/json_per_langue_label/"+debut+"/"+fin
+      this.apiUrl_per_label_true = "http://127.0.0.1:5000/number_true/"+debut+"/"+fin
+      this.apiUrl_per_label_false = "http://127.0.0.1:5000/number_false/"+debut+"/"+fin
+      this.apiUrl_per_label_mixture = "http://127.0.0.1:5000/number_mixture/"+debut+"/"+fin
+      this.apiUrl_per_label_other = "http://127.0.0.1:5000/number_other/"+debut+"/"+fin
     }
-    this.apiUrl_top_entity_graph = "http://127.0.0.1:5000/json_per_entity_date1_date2/"+debut+"/"+fin
-    this.apiUrl_per_source_label = "http://127.0.0.1:5000/json_born_per_source_label/"+debut+"/"+fin
-    this.apiUrl_per_langue = "http://127.0.0.1:5000/json_per_langue_label/"+debut+"/"+fin
-    this.apiUrl_per_label_true = "http://127.0.0.1:5000/number_true/"+debut+"/"+fin
-    this.apiUrl_per_label_false = "http://127.0.0.1:5000/number_false/"+debut+"/"+fin
-    this.apiUrl_per_label_mixture = "http://127.0.0.1:5000/number_mixture/"+debut+"/"+fin
-    this.apiUrl_per_label_other = "http://127.0.0.1:5000/number_other/"+debut+"/"+fin
+    else{
+      if(debut.length>0 && fin.length>0){
+        this.apiUrl_first_graph = "http://127.0.0.1:5000/json_per_date1_label?entity="+selectedOption.toString()+"&date1="+debut+"&date2="+fin+"&granularite"+granularite
+      }else{
+        this.apiUrl_first_graph = "http://127.0.0.1:5000/json_per_date1_label?entity="+selectedOption.toString()+"&date1=1996&date2=2024&granularite="+granularite
+      }
+  
+      this.apiUrl_top_entity_graph = "http://127.0.0.1:5000/json_per_entity_date1_date2/"+selectedOption.toString()+"/"+debut+"/"+fin
+      this.apiUrl_per_source_label = "http://127.0.0.1:5000/json_born_per_source_label/"+selectedOption.toString()+"/"+debut+"/"+fin
+      this.apiUrl_per_langue = "http://127.0.0.1:5000/json_per_langue_label/"+selectedOption.toString()+"/"+debut+"/"+fin
+      this.apiUrl_per_label_true = "http://127.0.0.1:5000/number_true/"+selectedOption.toString()+"/"+debut+"/"+fin
+      this.apiUrl_per_label_false = "http://127.0.0.1:5000/number_false/"+selectedOption.toString()+"/"+debut+"/"+fin
+      this.apiUrl_per_label_mixture = "http://127.0.0.1:5000/number_mixture/"+selectedOption.toString()+"/"+debut+"/"+fin
+      this.apiUrl_per_label_other = "http://127.0.0.1:5000/number_other/"+selectedOption.toString()+"/"+debut+"/"+fin
+    }
 
     this.submitTriggeredSource.next();
     this.submitTriggeredTopEntity.next();
